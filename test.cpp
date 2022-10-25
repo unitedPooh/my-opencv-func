e44c81c7226b178563e238ed68b0d659f8d72714
united_pooh <united_pooh@outlook.com>
2022-10-25 16:13:40 +0800

    添加测试文件

--- test.cpp
+++ test.cpp
@@ -1,3 +1,17 @@
 //
 // Created by atri on 22-10-25.
 //
+#include<iostream>
+#include<opencv4/opencv2/opencv.hpp>
+
+#include "func.hpp"
+
+using namespace std;
+using namespace cv;
+int main(){
+	cv::Mat image_ = imread("/home/atri/c++projects/MyOpencvFunc/test.png");
+	cv::imshow("ATRI",image_);
+	waitKey(0);
+	destroyAllWindows();
+	return 0;
+}
